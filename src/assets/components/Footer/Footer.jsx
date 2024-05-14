bb68aba7c0662312742cd3310057806131ae3c19
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-05-14 11:37:38 +0530

    final check

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -73,7 +73,7 @@ export default function Footer() {
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        © 2024 
                         {/* <a href="https://hiteshchoudhary.com/" className="hover:underline"> */}
                             Divyansh Agarwal
                         {/* </a> */}
